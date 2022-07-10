340a735b248290b49d4bab8064b002dd993fd36a
elaine460 <102460032+elaine460@users.noreply.github.com>
2022-07-10 21:33:55 +0800

    webhostingPHP

---
+++ php (1)/php/login_user.php
@@ -0,0 +1,48 @@
+<?php
+if (!isset($_POST)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
+
+include_once("dbconnect.php");
+$email = $_POST['email'];
+$password = sha1($_POST['password']);
+$sqllogin = "SELECT * FROM tbl_users WHERE user_email = '$email' AND user_password = '$password'";
+$result = $conn->query($sqllogin);
+$numrow = $result->num_rows;
+
+if ($numrow > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $userlist['id'] = $row['user_id'];
+        $userlist['email'] = $row['user_email'];
+        $userlist['password'] = $row['user_password'];
+        $userlist['name'] = $row['user_name'];
+        $userlist['phone'] = $row['user_phone'];
+        $userlist['address'] = $row['user_address'];
+    }
+    $sqlgetqty = "SELECT * FROM tbl_cart WHERE user_email = '$email' AND cart_status IS NULL";
+    $result = $conn->query($sqlgetqty);
+    $number_of_result = $result->num_rows;
+    $carttotal = 0;
+    while($row = $result->fetch_assoc()) {
+        $carttotal = $row['cart_qty'] + $carttotal;
+    }
+    $mycart = array();
+    $userlist['cart'] =$carttotal;
+
+    $response = array('status' => 'success', 'data' => $userlist);
+    sendJsonResponse($response);
+} else {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+}
+
+function sendJsonResponse($sentArray)
+{
+    header('Content-Type: application/json');
+    echo json_encode($sentArray);
+}
+?>
+
+
